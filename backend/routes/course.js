89c7c32841724adca7ebdb8e9fc693f467cd9ee6
Ramandeep Formay <raman@kidoodle.tv>
2024-03-13 00:06:15 -0600

    frontend boilerplate code

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -21,14 +21,24 @@ app.post("/create", auth, async (req,res,next)=>{
     if(existingCourseName){
         return res.status(411).json({msg:"Course already exists"})
     }
+    const createCourse = await Course.create({
+        title: req.body.title,
+        description: req.body.description
+    })
     return res.status(200).json("Course created successfully")
 }catch(error){
     next(error)
 }
 })
 
-app.get("/search",(req,res,next)=>{
-    
+app.get("/search", async(req,res,next)=>{
+        try{
+            const filterWord = req.params.filter  || ""
+            const courses = await Course.find({title: filterWord })
+            res.status(200).json({courses})
+        }catch(error){
+            next(error)
+        }
 })
 
 export default app
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState }  from 'react'
+
+
+
+function App() {
+  
+
+  return (
+   <>
+    <div></div>
+   </>
+  )
+}
+
+export default App
